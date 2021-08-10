d8acdb19d6ecd950325e2597ba19125d32125f7f
Timothy Vieira <vieira.timothy@yahoo.ca>
2021-08-10 01:03:41 -0400

    Added styling to messages to make overlap stop

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import {Avatar} from "@material-ui/core"
+
+function Header() {
+    return (
+        <div className="header">
+            <img 
+                className="header__icon" 
+                src="https://d1.awsstatic.com/product-marketing/AI/customers/Customers%20Page/600x400_Airbnb_Logo.f891e0066454456292ad7009aad136ea679f2cfd.png"
+                alt="airbnb logo"
+            />
+
+            <div className="header__center">
+                <input type="text" />
+                <SearchIcon />
+            </div>
+
+            <div className="header__right">
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+
+export default Header
